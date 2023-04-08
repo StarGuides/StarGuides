49e0e71fcc6c4df6fc23e2cd1335437afd2df0f3
Anne-lise Emig <anneliseemig@gmail.com>
2023-04-07 19:58:15 -0500

    started boilerplate

---
+++ server/server.js
@@ -0,0 +1,26 @@
+//import routers
+        //NOTE:ALWAYS SEND AN ARRAY
+        // front end needs data from sql in snake case
+
+
+// POST request for login path="/"
+
+    // if authenticated send dashboard DATA (if any) To front end
+    // if NOT logged in send a response letting the front end the customer needs to to sign up
+
+
+
+//parent component of Customer comp. makes a get request for all customer path="/dashboard
+
+
+
+
+//"path='analytics" get request for analytics page
+
+
+
+
+
+// get request for pending request path="/inquiries"
+
+
