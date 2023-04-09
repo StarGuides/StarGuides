87531bac046a064340f6e06eb9b1d53ee55ecfdf
Anne-lise Emig <anneliseemig@gmail.com>
2023-04-09 12:23:31 -0500

    refactored to account for fetching data

--- client/components/CustomerPage.jsx
+++ client/components/CustomerPage.jsx
@@ -4,11 +4,6 @@ import CustomerComponent from './CustomerComponent'
 import CustomerComponentHeader from './CustomerComponentHeader'
 
 const CustomerPage = () => {
-    // need to make a request to ./dashboard and load the data
-    // store data from response in state
-    // store (is loaded) as a boolean
-    // use useEffect to get data when page first loads
-
     // initialize customer data information
     const [customerData, setCustomerData] = useState([]);
     // boolean to store if data has been fetched
@@ -17,32 +12,41 @@ const CustomerPage = () => {
     // if data has not been fetched, get customer data and populate customerData state
     useEffect(() => {
       // if data has not been fetched, fetch it from database and update state
-      // if (!fetchedData) {
-      //   fetch("/dashboard")
-      //     .then((res) => res.json())
-      //     .then((customers) => {
-      //       setCustomerData(customers);
-      //       setFetchedData(true);
-      //     });
-      // }
+      if (!fetchedData) {
+        fetch("/dashboard")
+          .then((res) => res.json())
+          .then((fetchedData) => {
+            // array to hold all jsx CustomerComponents
+            const customers = [];
+            // add each jsx component to array, passing in individual customer's data
+            fetchedData.forEach((el, index) => {
+              customers.push(<CustomerComponent key = {index} info = {el}/>)
+            })
+            // set state customer data to customers
+            setCustomerData(customers);
+            // set fetched data to true
+            setFetchedData(true);
+          });
+      }
 
-      // get array of customers through fetch request
-      const fetchedData = [{first_name: 'al', last_name: 'emig', email_address: 'test', phone_number: 'test', location: 'test', start_date: 'test', end_date: 'test', skill_lvl: 'test', destination: 'test'}]
-      const customers = [];
-      // get array of customer components
-      fetchedData.forEach((el, index) => {
-        customers.push(<CustomerComponent 
-          key = {index}
-          info = {el}
-        />)
-      })
-      // set state to array of customer components
-      setCustomerData(customers);
+      // TESTING WITH MOCK DATA
+      // // get array of customers through fetch request
+      // const fetchedData = [{first_name: 'al', last_name: 'emig', email_address: 'test', phone_number: 'test', location: 'test', start_date: 'test', end_date: 'test', skill_lvl: 'test', destination: 'test'}]
+      // const customers = [];
+      // // get array of customer components
+      // fetchedData.forEach((el, index) => {
+      //   customers.push(<CustomerComponent 
+      //     key = {index}
+      //     info = {el}
+      //   />)
+      // })
+      // // set state to array of customer components
+      // setCustomerData(customers);
     }, []);
 
     return (
         // return customer header
-        // return all customer components
+        // return all customer components from the state
         <>
         <CustomerComponentHeader />
         {customerData}
